27b8a14a8020c5557f455becbe029ead3612e8d1
LoreLosi72 <lorenzo.losi.studenti@isii.it>
2024-11-13 17:20:40 +0100

    Made database connection a function

--- config/connection-database.js
+++ config/connection-database.js
@@ -1,19 +1,23 @@
 // import mysql package
 let mysql = require('mysql');
 
-// configuring database connection parameters
-let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'liveluxe'
-});
+// Function for create connection to database
+function createConnection() {
+    // configuring database connection parameters
+    let connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'liveluxe'
+    });
 
-// database connection
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected to mySQL database.")
-});
+    // database connection
+    connection.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected to mySQL database.")
+    });
 
-// close database connection
-connection.end();
+    return connection;
+}
+
+module.exports = { createConnection };
