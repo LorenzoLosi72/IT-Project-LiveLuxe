92fa7f9a1af57b4c05b3189088870e2421f7aeb4
LoreLosi72 <lorenzo.losi.studenti@isii.it>
2024-11-15 01:09:02 +0100

    Scripts for react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LiveLuxeNavbar from './components/Navbar.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App(){
+    return (
+        <div>
+           <LiveLuxeNavbar/>
+        </div>
+    );
+};
+
+export default App;
+
